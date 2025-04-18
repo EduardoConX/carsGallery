88dc8ec36d2dac7f103990d16bf6259006623973
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-04-18 16:32:42 -0600

    Improve pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
 import useCars from './composables/getCars'
+import PaginationButton from './components/PaginationButton.vue'
 
 const { cars, isLoading, error, isError } = useCars()
 const searchQuery = ref('')
@@ -31,24 +32,16 @@ const handlePageChange = (pageNumber: number) => {
       <div v-if="isLoading" class="text-center text-white">Loading...</div>
       <div v-else-if="isError" class="text-center text-red-500">{{ error }}</div>
       <div v-else>
-        <input
-          type="text"
+        <input type="text"
           class="block w-full pl-10 pr-3 py-2 border border-gray-700 rounded-lg bg-gray-800 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:border-transparent mb-12"
-          placeholder="Search"
-          :value="searchQuery"
-          @input="
+          placeholder="Search" :value="searchQuery" @input="
             (e) => {
               searchQuery = (e.target as HTMLInputElement).value
               currentPage = 1
             }
-          "
-        />
+          " />
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          <div
-            v-for="(car, index) in currentCars"
-            :key="index"
-            class="bg-gray-800 rounded-lg overflow-hidden"
-          >
+          <div v-for="(car, index) in currentCars" :key="index" class="bg-gray-800 rounded-lg overflow-hidden">
             <img :src="car.image" alt="" class="w-full h-48 object-cover" />
             <div class="p-6">
               <h2 class="text-2xl font-bold text-white mb-4">{{ car.name }}</h2>
@@ -67,19 +60,10 @@ const handlePageChange = (pageNumber: number) => {
         </div>
 
         <div v-if="totalPages > 1" class="flex justify-center mt-12 space-x-2">
-          <button
-            v-for="pageNumber in totalPages"
-            :key="pageNumber"
-            @click="handlePageChange(pageNumber)"
-            :class="[
-              'px-4 py-2 rounded-lg transition-colors',
-              currentPage === pageNumber
-                ? 'bg-emerald-600 text-white'
-                : 'bg-gray-700 text-gray-300 hover:bg-gray-600',
-            ]"
-          >
-            {{ pageNumber }}
-          </button>
+          <PaginationButton @click="handlePageChange(currentPage - 1)" :is-disabled="currentPage === 1" text="Prev" />
+          <PaginationButton @click="handlePageChange(currentPage + 1)" :is-disabled="currentPage === totalPages"
+            text="Next" />
+
         </div>
       </div>
     </div>
---
+++ src/components/PaginationButton.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+defineProps<{
+  isDisabled: boolean
+  text: string
+}>()
+</script>
+
+<template>
+  <button :class="[
+    'px-4 py-2 rounded-lg transition-colors',
+    isDisabled
+      ? 'bg-gray-700 text-gray-300 hover:bg-gray-600 cursor-not-allowed'
+      : 'bg-emerald-600 hover:bg-emerald-700 text-white cursor-pointer'
+  ]" :disabled="isDisabled">{{ text }}</button>
+</template>
